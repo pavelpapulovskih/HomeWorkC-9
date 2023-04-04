cc125b628f0e0bc08d8256518a67a77fa1701c0c
Your Pavel <p9l-real-chel@mail.ru>
2023-04-04 22:14:03 +0500

    HomeWork

---
+++ Work64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string NumbRec(int n)
+{
+    if (n == 0) return "";
+    return n + " " + NumbRec(n - 1);
+}
+
+
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumbRec(n));
---
+++ Work68/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int LeviAckermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return LeviAckermann(m - 1, 1);
+    }
+    else
+    {
+        return LeviAckermann(m - 1, LeviAckermann(m, n - 1));
+    }
+}
+
+
+Console.WriteLine("Введите натуральное не отрицательное число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное не отрицательное число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана принимает значения {m} и {n} и даёт результат {LeviAckermann(m, n)}");
